569b4250161cf47584fcd5e7e9260277fd77e217
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2016-09-07 04:07:08 -0300

    Adding tag on _push.bat

--- _push.bat
+++ _push.bat
@@ -1,1 +1,3 @@
+docker tag luizcarlosfaria/nginx-pagespeed:latest luizcarlosfaria/nginx-pagespeed:nginx-1.10.1--pagespeed-1.11.33.3
 docker push luizcarlosfaria/nginx-pagespeed:latest
+docker push luizcarlosfaria/nginx-pagespeed:nginx-1.10.1--pagespeed-1.11.33.3
